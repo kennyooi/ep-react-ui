6a2d2235b1d0c4b2ba58810f8463ab1d8f08693a
Kenny <kenny@seeties.me>
2017-11-17 19:09:48 +0800

    [fix] Invalid promise using inside GeoAutocomplete component.

--- src/components/Autocomplete/GeoAutocomplete.js
+++ src/components/Autocomplete/GeoAutocomplete.js
@@ -4,6 +4,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import Promise from 'bluebird';
 import SelectAutocomplete from './SelectAutocomplete';
 
 import './GeoAutocomplete.less';
